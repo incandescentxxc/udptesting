96c9ed56e52ef014872c73a529ac13c1d3922b96
incandescentxxc <incandescentxxc@gmail.com>
2020-02-11 08:17:39 +0800

    psnr script version 2.0, refine the calculation of psnr, lower the definition of the video

--- test_mp.py
+++ test_mp.py
@@ -5,7 +5,7 @@ import time
 import csv
 def run_flow(rate, mode):
 	
-	NIC1ip = '192.168.1.44' # server IP
+	NIC1ip = '192.168.1.45' # server IP
 	LOClip = '192.168.1.199'
 	NIC1 = 'ens33' # sending Interface 
 	target_NIC = 'ens33' # modifying interface 
@@ -14,32 +14,31 @@ def run_flow(rate, mode):
 	if(mode == 0):# udp
 		outputVideoName = 'udpvideo' + str(rate) + '.mp4'
 		outputStatsName = 'udppsnr' + str(rate) + '.log'
-		cmd_exec_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -f h264 -i udp://192.168.1.44:8000?timeout=15000000 '+outputVideoName
-		cmd_cal_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -i Trial1_55M.mp4 -i '+ outputVideoName + ' -lavfi psnr="stats_file=psnr.log" -f null -'
-		cmd_tran_remote_server = 'scp incandescentxxc@' + NIC1ip + ':psnr.log '+outputStatsName
+		cmd_exec_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -f h264 -i udp://192.168.1.45:8000?timeout=15000000 '+outputVideoName
+
 	else: # udplite
 		outputVideoName = 'udplitevideo' + str(rate) + '.mp4'
 		outputStatsName = 'udplitepsnr' + str(rate) + '.log'
-		cmd_exec_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -f h264 -i udplite://192.168.1.44:8000?timeout=15000000 '+outputVideoName
-		cmd_cal_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -i Trial1_55M.mp4 -i '+ outputVideoName + ' -lavfi psnr="stats_file=psnr.log" -f null -'
-		cmd_tran_remote_server = 'scp incandescentxxc@' + NIC1ip + ':psnr.log '+outputStatsName
+		cmd_exec_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -f h264 -i udplite://192.168.1.45:8000?timeout=15000000 '+outputVideoName
+
+	cmd_cal_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffmpeg -i video_480.mp4 -i '+ outputVideoName + ' -lavfi psnr="stats_file=psnr.log" -f null -'
+	cmd_getFPS_remote_server = 'ssh incandescentxxc@' + NIC1ip + ' ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 ' + outputVideoName + ' > numberFrame.txt'
+	cmd_tran_remote_server = 'scp incandescentxxc@' + NIC1ip + ':psnr.log '+outputStatsName
 	
 	# flow starts here
-	if(rate != 0):
+	if(rate != 0): # adjust the corruption rate
 		os.system("sudo tc qdisc add dev " + target_NIC + " root netem corrupt " + str(rate))
-	# os.system("cd /home/xuxiaochuan/CodesHQ/UDPVideo")
-	# remote_server = subprocess.Popen(cmd_start_remote_server, shell=True, stdout=subprocess.PIPE)
-	# remote_server.wait()
-	# time.sleep(5)
 	remote_server_exec = subprocess.Popen(cmd_exec_remote_server, shell=True, stdout=subprocess.PIPE)
 	time.sleep(6) # wait for the server to start listening
 	if(mode == 0): # udp
-		os.system("ffmpeg -re -i Trial1_55M.mp4 -f h264 udp://192.168.1.44:8000")
+		os.system("ffmpeg -re -i video_480.mp4 -f h264 udp://192.168.1.45:8000")
 	else: #udplite
-		os.system("ffmpeg -re -i Trial1_55M.mp4 -f h264 udplite://192.168.1.44:8000")
-	# remote_server_exec.wait()
-	remote_server_cal = subprocess.Popen(cmd_cal_remote_server, shell=True, stdout=subprocess.PIPE)
+		os.system("ffmpeg -re -i video_480.mp4 -f h264 udplite://192.168.1.45:8000")
+	remote_server_exec.wait()
+	remote_server_cal = subprocess.Popen(cmd_cal_remote_server, shell=True, stdout=subprocess.PIPE) # calculate psnr
 	remote_server_cal.wait()
+	remote_server_fps = subprocess.Popen(cmd_getFPS_remote_server, shell=True, stdout=subprocess.PIPE) # get the number of frames
+	remote_server_fps.wait()
 	remote_server_tra = subprocess.Popen(cmd_tran_remote_server, shell=True, stdout=subprocess.PIPE)
 	remote_server_tra.wait()
 	if(rate != 0):
@@ -53,17 +52,24 @@ def run_exp(rate, mode):
 		outputStatsName = 'udppsnr' + str(rate) + '.log'
 	else:
 		outputStatsName = 'udplitepsnr' + str(rate) + '.log'
+
+	with open("numberFrame.txt", "r") as paraFile:
+		num = int(paraFile.readline())
 	with open(outputStatsName, 'r') as infile:
-		counter = 0
 		psnr_ys = 0
-		for line in infile:
+		counter = 0
+		for i in range(0, num):
+			line = infile.readline()
+			if(line == ''): # may be occurred when the frame number got exceeds the actual frame number
+				print("Empty line!")
+				return psnr_ys/counter
 			lineContents = line.split(" ")
 			psnr = lineContents[6].split(":")
 			psnr_y = float(psnr[1])
 			psnr_ys += psnr_y
 			counter += 1
 	
-	psnr_avg = psnr_ys / counter
+	psnr_avg = psnr_ys / num
 	print("Counter is: " + str(counter) +"\n")
 	print("psnr_y_avg:" + str(psnr_avg) +"\n")
 	return psnr_avg
@@ -78,12 +84,13 @@ def run_exp(rate, mode):
 if __name__ == "__main__":
 	udpDict = {}
 	udpLiteDict = {}
-	for bit_error in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
+	for bit_error in [1]:
 		udpDict[str(bit_error)] = run_exp(bit_error, 0)
 		udpLiteDict[str(bit_error)] = run_exp(bit_error, 1)
+	
+	print("UDP: \n")
 	print(udpDict)
-	print("\n")
-	print("\n")
+	print("UDPLite: \n")
 	print(udpLiteDict)
 	with open('udp.csv','w',newline="") as csv_file:
 		writer = csv.writer(csv_file)
